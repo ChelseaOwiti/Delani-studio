1780b949e08931f8eda7b6a40cf149479882dfd9
Chelsea Owiti <chelseaowiti@gmail.com>
2019-05-05 22:09:42 -0400

    toggling effect on 'WHAT WE DO' section added

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// TOGGLING ON WHAT WE DO
+$(document).ready(function(){
+  $("#a").click(function(){
+    $("#icon").toggle();
+    $(".hidden").toggle();
+  });
+  $("#b").click(function(){
+    $("#icon2").toggle();
+    $(".hidden2").toggle();
+  });
+  $("#c").click(function(){
+    $("#icon3").toggle();
+    $(".hidden3").toggle();
+  });
+});
